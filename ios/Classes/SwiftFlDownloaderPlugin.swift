12c90a890398b1d1e1a4ede9a21c5f4c1bf1b09d
Pedro Carvalho <17789155+inceptusp@users.noreply.github.com>
2022-05-17 10:08:55 -0300

    Added base swift code

--- ios/Classes/SwiftFlDownloaderPlugin.swift
+++ ios/Classes/SwiftFlDownloaderPlugin.swift
@@ -3,12 +3,21 @@ import UIKit
 
 public class SwiftFlDownloaderPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "fl_downloader", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: "dev.inceptusp.fl_downloader", binaryMessenger: registrar.messenger())
     let instance = SwiftFlDownloaderPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    if call.method == "download" {
+      result("iOS " + UIDevice.current.systemVersion)
+    } else if call.method == "openFile" {
+      openFile()
+      result(nil)      
+    } else {
+      result(FlutterMethodNotImplemented)
+    }
   }
+
+  public func openFile() {}
 }
