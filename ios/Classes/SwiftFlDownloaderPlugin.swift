4b51e75cd51d638e7b06619b1bec64f16285c800
Pedro Carvalho <17789155+inceptusp@users.noreply.github.com>
2022-05-17 16:16:31 -0300

    Added custom URLSession

--- ios/Classes/SwiftFlDownloaderPlugin.swift
+++ ios/Classes/SwiftFlDownloaderPlugin.swift
@@ -2,6 +2,10 @@ import Flutter
 import UIKit
 
 public class SwiftFlDownloaderPlugin: NSObject, FlutterPlugin {
+  private lazy var urlSession = URLSession(configuration: .default,
+                                           delegate: self,
+                                           delegateQueue: nil)
+
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "dev.inceptusp.fl_downloader", binaryMessenger: registrar.messenger())
     let instance = SwiftFlDownloaderPlugin()
@@ -10,14 +14,55 @@ public class SwiftFlDownloaderPlugin: NSObject, FlutterPlugin {
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     if call.method == "download" {
-      result("iOS " + UIDevice.current.systemVersion)
+      download(call.arguments["url"] as! String,
+               call.arguments["headers"] as! [String: String],
+               call.arguments["fileName"] as! String)
+      result(nil)
     } else if call.method == "openFile" {
       openFile()
-      result(nil)      
+      result(nil)
+    } else if call.method == "cancel" {
+      cancel()
+      result(nil)
     } else {
       result(FlutterMethodNotImplemented)
     }
   }
 
+  public func download(url: String, headers: [String: String]?, fileName: String) {
+    let request: URLRequest = {
+      var request = URLRequest(url: URL(string: url)!)
+      for (key, value) in headers ?? [:] {
+        request.addValue(value, forHTTPHeaderField: key)
+      }
+      return request
+    }()
+
+    let downloadTask = urlSession.downloadTask(with: request) {
+      urlOrNil, responseOrNil, errorOrNil in
+      // check for and handle errors:
+      // * errorOrNil should be nil
+      // * responseOrNil should be an HTTPURLResponse with statusCode in 200..<299
+      
+      guard let fileURL = urlOrNil else { return }
+      do {
+          let documentsURL = try
+              FileManager.default.url(for: .documentDirectory,
+                                      in: .userDomainMask,
+                                      appropriateFor: nil,
+                                      create: true)
+          let savedURL = documentsURL.appendingPathComponent(fileName ?? fileURL.lastPathComponent)
+          try FileManager.default.moveItem(at: fileURL, to: savedURL)
+      } catch {
+          print ("file error: \(error)")
+      }
+    }
+    downloadTask.resume()
+  }
+
   public func openFile() {}
+
+  public func cancel() {}
+
+  private func trackProgress() {}
 }
